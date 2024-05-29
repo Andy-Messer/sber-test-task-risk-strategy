fe0518534cad7f3bdbe0257f2133905330e3a748
a.i.krotov <a.i.krotov@tinkoff.ru>
2024-05-29 09:37:24 +0300

    edit py-env

--- .history/install_20240529093632.sh
+++ .history/install_20240529093632.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 echo "Installing..."
-
 python3 -m venv env
-source ./env/bin/activate
-pip install -r requirements.txt
-
+source ./py-env/bin/activate
 echo "Installation completed!"
--- .history/install_20240529093700.sh
+++ .history/install_20240529093700.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "Installing..."
-
 python3 -m venv env
 source ./env/bin/activate
 pip install -r requirements.txt
-
 echo "Installation completed!"
